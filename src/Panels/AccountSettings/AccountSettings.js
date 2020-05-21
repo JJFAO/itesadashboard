2f2d1981afd5d7f9a8b855bcb0a225f98e6be96d
Juan <jjfalonso@gmail.com>
2020-05-20 23:26:39 -0300

    AccountSettings design completed, integration with firebase and color palette input required

--- src/Panels/AccountSettings/AccountSettings.js
+++ src/Panels/AccountSettings/AccountSettings.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Button, Input } from 'antd';
+import AnchorLink from 'antd/lib/anchor/AnchorLink';
+import styles from './accountsettings.module.scss'
 
-const AccountSettings = () => {
+const AccountSettings = ({ userID }) => {
+
+    const handleClick = () => {
+        console.log('w');
+    }
+
+    const handleSave = () => {
+        console.log('s');
+    }
+    const userUrl = userID && userID.replace(/\s/g, '').toLowerCase();
     return (
-        <div>
-           Mi Cuenta 
+        <div className={styles.AccountSettings}>
+            <div className={styles.userUrl}>
+                <p>
+                    Link a tu negocio:{' '}
+                    <a href={`http://checkout.itesa.co/${userUrl}`} target="_blank" rel="noopener noreferrer">
+                        <span>http://checkout.itesa.co/{userUrl}</span>
+                    </a>
+                </p>
+            </div>
+            <div>
+                <Button onClick={handleClick} className={styles.btnWhats}>
+                    Compartir en WhatsApp
+                </Button>
+            </div>
+            <div>
+                <p>Color de Tema:</p>
+                <Input type="color" className={styles.inputColor}></Input>
+            </div>
+            <div>
+                <Button onClick={handleSave} type="primary" className={styles.btnSave}>
+                    Guardar Cambios
+                </Button>
+            </div>
         </div>
     );
 };
